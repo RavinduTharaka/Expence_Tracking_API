821e9b5df1bd693f48f65cc8ccc0e84ffcde62ce
RavinduTharaka <ravi.tharaka96@gmail.com>
2023-09-30 01:46:31 +0530

    User Authentication

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ExpenceController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource("categories",CategoryController::class);
+Route::apiResource("categories",ExpenceController::class);
+
+Route::post("login",[UserController::class,'index']);
